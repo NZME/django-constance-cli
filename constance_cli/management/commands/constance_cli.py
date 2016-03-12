9557e38c280164ffd5719d7244dc5a90faab48bb
John Carter <john@therefromhere.org>
2016-03-12 19:13:12 +1300

    moved get/set to a utils module

--- constance_cli/management/commands/constance_cli.py
+++ constance_cli/management/commands/constance_cli.py
@@ -3,8 +3,7 @@ from __future__ import unicode_literals
 from __future__ import print_function
 from django.core.management import BaseCommand, CommandError
 from django.utils.translation import ugettext_lazy as _
-from constance import config, settings
-from constance.admin import ConstanceForm
+from ...utils import get_constance_values, get_constance_value, set_constance_value
 
 
 class Command(BaseCommand):
@@ -18,33 +17,6 @@ class Command(BaseCommand):
         group.add_argument("--get", dest='setting', nargs=1)
         group.add_argument("--set", dest='setting', nargs=2, metavar=('SETTING', 'VALUE',))
 
-    def _get_constance_values(self):
-        # copied from constance.admin
-        default_initial = ((name, default)
-            for name, (default, help_text) in settings.CONFIG.items())
-        # Then update the mapping with actually values from the backend
-        initial = dict(default_initial,
-            **dict(config._backend.mget(settings.CONFIG.keys())))
-
-        return initial
-
-    def _get_constance_field(self, name):
-        form = ConstanceForm(initial=self._get_constance_values())
-
-        return form.fields[name]
-
-    def _set_constance_value(self, raw_name, raw_value):
-        """
-        Parses and sets a Constance value
-        :param raw_name:
-        :param raw_value:
-        :return:
-        """
-        field = self._get_constance_field(raw_name)
-
-        value = field.clean(field.to_python(raw_value))
-        setattr(config, raw_name, value)
-
     def handle(self, *args, **options):
         if options.get('setting'):
             raw_name = options.get('setting')[0]
@@ -52,7 +24,7 @@ class Command(BaseCommand):
             if len(options.get('setting')) == 1:
                 # get
                 try:
-                    print(getattr(config, raw_name))
+                    print(get_constance_value(raw_name))
                 except AttributeError as e:
                     raise CommandError(raw_name + _(" is not defined in settings.CONSTANCE_CONFIG"))
             else:
@@ -60,10 +32,10 @@ class Command(BaseCommand):
                 raw_value = options.get('setting')[1]
 
                 try:
-                    self._set_constance_value(raw_name, raw_value)
+                    set_constance_value(raw_name, raw_value)
                 except KeyError as e:
                     raise CommandError(raw_name + _(" is not defined in settings.CONSTANCE_CONFIG)"))
 
         elif options.get('list'):
-            for k, v in self._get_constance_values().items():
+            for k, v in get_constance_values().items():
                 print(k, v, sep="\t")
